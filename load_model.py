2ee07a4f8f13bfd2ced4decb32bef3461292aef0
lsmierzchalski <lsmierzchalski@gmail.com>
2020-02-15 14:12:04 +0100

    add load model and predict in real time

--- load_model.py
+++ load_model.py
@@ -3,6 +3,29 @@ import cv2
 from keras.models import load_model
 from keras import Model
 
+cards = ["ACE", "KING", "JACK", "NINE", "QUEEN", "TEN"]
+
+def list_cards_to_str(list):
+    result = ""
+    for i in range(len(cards)):
+        result += cards[i] + ": " + format(float(list[0][i]), ".2f") + ", "
+    return result
+
+def get_max_index(list):
+    max_index = 0
+    max_value = 0.0
+    for i in range(len(cards)):
+        if float(list[0][i]) > max_value:
+            max_index = i
+            max_value = float(list[0][i])
+    return max_index, max_value
+
+def get_result_text(list):
+    index, value = get_max_index(list)
+    text = cards[index] + " (" + str(value * 100) + "%)"
+    return text
+
+
 print("loading model")
 model: Model = load_model('cards_model.hdf5')
 print("model is loaded")
@@ -18,10 +41,13 @@ while(True):
     resized = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
     image = np.expand_dims(resized, axis=0)
     result = model.predict(image)
-    print(result)
+    #print(list_cards_to_str(result))
+    text = get_result_text(result)
+    cv2.putText(frame, text,
+                (10,10),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
 
     # Display the resulting frame
-    cv2.imshow('frame',resized)
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
